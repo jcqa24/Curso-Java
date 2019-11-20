4a571729e704a57021ddabec8b45de83fb408837
Juan Carlos Quiroz Aguilar <jcqa24@gmail.com>
2019-11-20 16:19:25 -0600

    Capitulo 2 Variables

---
+++ Variables/src/variables/Variables.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variables;
+
+/**
+ *
+ * @author juanc
+ */
+public class Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // byte es numero -128 a 127 
+        byte tuEdad = 40;
+        //short -32768 a 32767
+        short Salario = 25000;
+        System.out.println(Salario);
+        // int numero enteros grandes
+        int edad = 50;
+       // long numero mas grandes que int 
+       long longitudDeLaTierra = 12742000;
+       System.out.println(longitudDeLaTierra);
+       // float numeros con punto decimal
+       float valorPi = 3.1416f ;
+       System.out.println(valorPi);
+       // Double numeros con punto decimal 
+       double promedio = 8.9 ;
+       System.out.println(promedio);
+       //char un caracter 
+       char inicial = '%';
+       System.out.println(inicial);
+       
+    }
+    
+}
